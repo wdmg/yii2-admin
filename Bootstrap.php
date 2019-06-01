773faab3b652ec9ee2d15e87c8fffe5e2c85f0fe
alex-wdmg <wdmg.com.ua@gmail.com>
2019-06-02 02:34:38 +0300

    Init submodules

--- Bootstrap.php
+++ Bootstrap.php
@@ -10,31 +10,165 @@ namespace wdmg\admin;
 
 use yii\base\BootstrapInterface;
 use Yii;
+use yii\helpers\ArrayHelper;
 
 
 class Bootstrap implements BootstrapInterface
 {
 
-    public $modules = [
-        "wdmg/yii2-activity",
-        "wdmg/yii2-api",
-        "wdmg/yii2-bookmarks",
-        "wdmg/yii2-comments",
-        "wdmg/yii2-forms",
-        "wdmg/yii2-geo",
-        "wdmg/yii2-likes",
-        "wdmg/yii2-messages",
-        "wdmg/yii2-options",
-        "wdmg/yii2-rbac",
-        "wdmg/yii2-reposts",
-        "wdmg/yii2-reviews",
-        "wdmg/yii2-services",
-        "wdmg/yii2-stats",
-        "wdmg/yii2-tasks",
-        "wdmg/yii2-tickets",
-        "wdmg/yii2-users",
-        "wdmg/yii2-views",
-        "wdmg/yii2-votes",
+    public $packages = [
+        "wdmg/yii2-activity" => [
+            'moduleId' => 'activity',
+            'moduleClass' => 'wdmg\activity\Module',
+            'bootstrapClass' => 'wdmg\activity\Bootstrap',
+            'moduleOptions' => [
+                'routePrefix' => 'admin'
+            ],
+        ],
+        "wdmg/yii2-api" => [
+            'moduleId' => 'api',
+            'moduleClass' => 'wdmg\api\Module',
+            'bootstrapClass' => 'wdmg\api\Bootstrap',
+            'moduleOptions' => [
+                'routePrefix' => 'admin'
+            ],
+        ],
+        "wdmg/yii2-bookmarks" => [
+            'moduleId' => 'bookmarks',
+            'moduleClass' => 'wdmg\bookmarks\Module',
+            'bootstrapClass' => 'wdmg\bookmarks\Bootstrap',
+            'moduleOptions' => [
+                'routePrefix' => 'admin'
+            ],
+        ],
+        "wdmg/yii2-comments" => [
+            'moduleId' => 'comments',
+            'moduleClass' => 'wdmg\comments\Module',
+            'bootstrapClass' => 'wdmg\comments\Bootstrap',
+            'moduleOptions' => [
+                'routePrefix' => 'admin'
+            ],
+        ],
+        "wdmg/yii2-forms" => [
+            'moduleId' => 'forms',
+            'moduleClass' => 'wdmg\forms\Module',
+            'bootstrapClass' => 'wdmg\forms\Bootstrap',
+            'moduleOptions' => [
+                'routePrefix' => 'admin'
+            ],
+        ],
+        "wdmg/yii2-geo" => [
+            'moduleId' => 'geo',
+            'moduleClass' => 'wdmg\geo\Module',
+            'bootstrapClass' => 'wdmg\geo\Bootstrap',
+            'moduleOptions' => [
+                'routePrefix' => 'admin'
+            ],
+        ],
+        "wdmg/yii2-likes" => [
+            'moduleId' => 'likes',
+            'moduleClass' => 'wdmg\likes\Module',
+            'bootstrapClass' => 'wdmg\likes\Bootstrap',
+            'moduleOptions' => [
+                'routePrefix' => 'admin'
+            ],
+        ],
+        "wdmg/yii2-messages" => [
+            'moduleId' => 'messages',
+            'moduleClass' => 'wdmg\messages\Module',
+            'bootstrapClass' => 'wdmg\messages\Bootstrap',
+            'moduleOptions' => [
+                'routePrefix' => 'admin'
+            ],
+        ],
+        "wdmg/yii2-options" => [
+            'moduleId' => 'options',
+            'moduleClass' => 'wdmg\options\Module',
+            'bootstrapClass' => 'wdmg\options\Bootstrap',
+            'moduleOptions' => [
+                'routePrefix' => 'admin'
+            ],
+        ],
+        "wdmg/yii2-rbac" => [
+            'moduleId' => 'rbac',
+            'moduleClass' => 'wdmg\rbac\Module',
+            'bootstrapClass' => 'wdmg\rbac\Bootstrap',
+            'moduleOptions' => [
+                'routePrefix' => 'admin'
+            ],
+        ],
+        "wdmg/yii2-reposts" => [
+            'moduleId' => 'reposts',
+            'moduleClass' => 'wdmg\reposts\Module',
+            'bootstrapClass' => 'wdmg\reposts\Bootstrap',
+            'moduleOptions' => [
+                'routePrefix' => 'admin'
+            ],
+        ],
+        "wdmg/yii2-reviews" => [
+            'moduleId' => 'reviews',
+            'moduleClass' => 'wdmg\reviews\Module',
+            'bootstrapClass' => 'wdmg\reviews\Bootstrap',
+            'moduleOptions' => [
+                'routePrefix' => 'admin'
+            ],
+        ],
+        "wdmg/yii2-services" => [
+            'moduleId' => 'services',
+            'moduleClass' => 'wdmg\services\Module',
+            'bootstrapClass' => 'wdmg\services\Bootstrap',
+            'moduleOptions' => [
+                'routePrefix' => 'admin'
+            ],
+        ],
+        "wdmg/yii2-stats" => [
+            'moduleId' => 'stats',
+            'moduleClass' => 'wdmg\stats\Module',
+            'bootstrapClass' => 'wdmg\stats\Bootstrap',
+            'moduleOptions' => [
+                'routePrefix' => 'admin'
+            ],
+        ],
+        "wdmg/yii2-tasks" => [
+            'moduleId' => 'tasks',
+            'moduleClass' => 'wdmg\tasks\Module',
+            'bootstrapClass' => 'wdmg\tasks\Bootstrap',
+            'moduleOptions' => [
+                'routePrefix' => 'admin'
+            ],
+        ],
+        "wdmg/yii2-tickets" => [
+            'moduleId' => 'tickets',
+            'moduleClass' => 'wdmg\tickets\Module',
+            'bootstrapClass' => 'wdmg\tickets\Bootstrap',
+            'moduleOptions' => [
+                'routePrefix' => 'admin'
+            ],
+        ],
+        "wdmg/yii2-users" => [
+            'moduleId' => 'users',
+            'moduleClass' => 'wdmg\users\Module',
+            'bootstrapClass' => 'wdmg\users\Bootstrap',
+            'moduleOptions' => [
+                'routePrefix' => 'admin'
+            ],
+        ],
+        "wdmg/yii2-views" => [
+            'moduleId' => 'views',
+            'moduleClass' => 'wdmg\views\Module',
+            'bootstrapClass' => 'wdmg\views\Bootstrap',
+            'moduleOptions' => [
+                'routePrefix' => 'admin'
+            ],
+        ],
+        "wdmg/yii2-votes" => [
+            'moduleId' => 'votes',
+            'moduleClass' => 'wdmg\votes\Module',
+            'bootstrapClass' => 'wdmg\votes\Bootstrap',
+            'moduleOptions' => [
+                'routePrefix' => 'admin'
+            ],
+        ],
     ];
 
     public function bootstrap($app)
@@ -51,31 +185,32 @@ class Bootstrap implements BootstrapInterface
         }
 
         // Add module URL rules
+        $app->getUrlManager()->enablePrettyUrl = true;
+        $app->getUrlManager()->showScriptName = false;
+        $app->getUrlManager()->enableStrictParsing = true;
+        $app->getUrlManager()->suffix = '/';
         $app->getUrlManager()->addRules(
             [
-                $prefix . '<module:admin>' => '<module>/admin/index',
-                $prefix . '<module:admin>/<controller:\w+>' => '<module>/<controller>',
-                $prefix . '<module:admin>/<controller:\w+>/<action:[0-9a-zA-Z_\-]+>' => '<module>/<controller>/<action>',
-                $prefix . '<module:admin>/<controller:\w+>/<action:[0-9a-zA-Z_\-]+>/<id:\d+>' => '<module>/<controller>/<action>',
-                [
-                    'pattern' => $prefix . '<module:admin>/',
-                    'route' => '<module>/admin/index',
-                    'suffix' => ''
+                '/admin' => 'admin/dashboard/index',
+                '<module:admin>/<controller:\w+>' => '<module>/<controller>',
+                '<module:admin>/<submodule:\w+>/<controller:\w+>' => '<module>/<submodule>/<controller>',
+                '<module:admin>/<controller:\w+>/<action:\w+>' => '<module>/<controller>/<action>',
+                '<module:admin>/<submodule:\w+>/<controller:\w+>/<action:\w+>' => '<module>/<submodule>/<controller>/<action>',
+                /*[
+                    'pattern' => '<module:admin>/',
+                    'route' => '<module>/dashboard/index',
+                    'suffix' => '',
                 ], [
-                    'pattern' => $prefix . '<module:admin>/<controller:\w+>/',
+                    'pattern' => '<module:admin>/<controller:\w+>/',
                     'route' => '<module>/<controller>',
-                    'suffix' => ''
+                    'suffix' => '',
                 ], [
-                    'pattern' => $prefix . '<module:admin>/<controller:\w+>/<action:[0-9a-zA-Z_\-]+>/',
+                    'pattern' => '<module:admin>/<controller:\w+>/<action:\w+>',
                     'route' => '<module>/<controller>/<action>',
-                    'suffix' => ''
-                ], [
-                    'pattern' => $prefix . '<module:admin>/<controller:\w+>/<action:[0-9a-zA-Z_\-]+>/<id:\d+>/',
-                    'route' => '<module>/<controller>/<action>',
-                    'suffix' => ''
-                ]
+                    'suffix' => '',
+                ],*/
             ],
-            false
+            true
         );
 
         // Configure administrative panel
@@ -85,26 +220,36 @@ class Bootstrap implements BootstrapInterface
             ]
         ]);
 
+        // Loading all modules
+        if (!Yii::$app instanceof \yii\console\Application) {
+            $extensions = $module->module->extensions;
+            foreach ($extensions as $extension) {
+                if (array_key_exists($extension['name'], $this->packages)) {
 
+                    $package = (object)$this->packages[$extension['name']];
+                    $module->setModule($package->moduleId, ArrayHelper::merge([
+                    //Yii::$app->setModule($package->moduleId, ArrayHelper::merge([
+                        'class' => $package->moduleClass
+                    ], $package->moduleOptions));
 
-        $module = Yii::$app->getModule('admin');
-        $extensions = $module->module->extensions;
-        foreach ($extensions as $extension) {
-            if (in_array($extension['name'], $this->modules)) {
-                $module_id = substr($extension['name'], strpos($extension['name'], '-') + 1, strlen($extension['name']));
-                if (!empty($module_id)) {
-                    Yii::$app->setModule($module_id, [
-                        'class' => str_replace('Bootstrap','Module', $extension['bootstrap']),
-                        'routePrefix' => 'admin'
-                    ]);
-                    if (Yii::$app->hasModule($module_id)) {
-                        if ($module instanceof yii\base\BootstrapInterface)
-                            $module->bootstrap(Yii::$app);
+                    $installed = Yii::$app->getModule($package->moduleId);
+                    if ($installed) {
+
+                        // Configure dashboard
+                        $installed->layout = 'dashboard';
+                        $installed->layoutPath = '@wdmg/admin/views/layouts';
+                        //$installed->routePrefix = '';
+
+                        if ($installed instanceof yii\base\BootstrapInterface)
+                            $installed->bootstrap(Yii::$app);
                         else
-                            Yii::$app->bootstrap[] = $extension['bootstrap'];
+                            Yii::$app->bootstrap[] = $package->bootstrapClass;
                     }
                 }
             }
         }
+
+
+        //$module->layoutPath = '';
     }
 }
