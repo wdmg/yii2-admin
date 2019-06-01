4169208ed672b9c4504d194f11eb8062aceecb2c
alex-wdmg <wdmg.com.ua@gmail.com>
2019-06-01 23:37:24 +0300

    Scan and loading modules (draft)

--- Bootstrap.php
+++ Bootstrap.php
@@ -14,6 +14,29 @@ use Yii;
 
 class Bootstrap implements BootstrapInterface
 {
+
+    public $modules = [
+        "wdmg/yii2-activity",
+        "wdmg/yii2-api",
+        "wdmg/yii2-bookmarks",
+        "wdmg/yii2-comments",
+        "wdmg/yii2-forms",
+        "wdmg/yii2-geo",
+        "wdmg/yii2-likes",
+        "wdmg/yii2-messages",
+        "wdmg/yii2-options",
+        "wdmg/yii2-rbac",
+        "wdmg/yii2-reposts",
+        "wdmg/yii2-reviews",
+        "wdmg/yii2-services",
+        "wdmg/yii2-stats",
+        "wdmg/yii2-tasks",
+        "wdmg/yii2-tickets",
+        "wdmg/yii2-users",
+        "wdmg/yii2-views",
+        "wdmg/yii2-votes",
+    ];
+
     public function bootstrap($app)
     {
         // Get the module instance
@@ -61,5 +84,27 @@ class Bootstrap implements BootstrapInterface
                 'class' => 'wdmg\admin\components\Dashboard'
             ]
         ]);
+
+
+
+        $module = Yii::$app->getModule('admin');
+        $extensions = $module->module->extensions;
+        foreach ($extensions as $extension) {
+            if (in_array($extension['name'], $this->installed)) {
+                $module_id = substr($extension['name'], strpos($extension['name'], '-') + 1, strlen($extension['name']));
+                if (!empty($module_id)) {
+                    Yii::$app->setModule($module_id, [
+                        'class' => str_replace('Bootstrap','Module', $extension['bootstrap']),
+                        'routePrefix' => 'admin'
+                    ]);
+                    if (Yii::$app->hasModule($module_id)) {
+                        if ($module instanceof yii\base\BootstrapInterface)
+                            $module->bootstrap(Yii::$app);
+                        else
+                            Yii::$app->bootstrap[] = $extension['bootstrap'];
+                    }
+                }
+            }
+        }
     }
 }
